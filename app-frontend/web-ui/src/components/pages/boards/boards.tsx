bee59a7af0652cde8308c718f2833ea94457c271
ngovanquoc <ngovanquoc480@gmail.com>
2025-07-29 13:27:40 +0700

    connect socket.io

--- app-frontend/web-ui/src/components/pages/boards/boards.tsx
+++ app-frontend/web-ui/src/components/pages/boards/boards.tsx
@@ -168,7 +168,7 @@ function BoardsPage() {
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {currentUser &&
-              boardList.boards.map((item, index) => (
+              boardList.map((item, index) => (
                 <Link key={index} to={`/boards/${item.id}/cards`}>
                   <div className="bg-gray-700 p-6 rounded-lg h-[130px] shadow-md hover:shadow-lg transition-shadow duration-200 cursor-pointer flex flex-col justify-between">
                     <h3 className="text-xl font-semibold mb-8 text-white">
